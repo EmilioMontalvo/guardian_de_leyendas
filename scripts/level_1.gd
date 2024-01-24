fd0204348eb8daf0882ae3bc0334826bafca45f5
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-01-24 00:22:53 -0500

    daño

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 @export var health = 100
+@export var actual_health = 100
+@onready var player=$Player
+@onready var hud=$Hud/HUD
+@onready var start_position=$StartPosition
 
 func _ready():
 	var enemies = get_tree().get_nodes_in_group("enemies")
@@ -9,3 +13,14 @@ func _ready():
 
 func _get_damage():
 	print("Damage")
+	health-=20	
+	hud.set_life_bar_value(health)
+	if health<=0:
+		reset_player()
+
+
+func reset_player():
+	player.velocity = Vector2.ZERO
+	player.global_position=start_position.global_position
+	health=100
+	hud.set_life_bar_value(health)
