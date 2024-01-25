25646b98210cf640832a7e8e054237d6c289a510
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-01-24 23:29:30 -0500

    modo tieso

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,29 +5,33 @@ class_name Player
 @export var gravity = 400
 @export var speed = 150
 @export var jumpforce = 200
-
 @onready var player = $AnimatedSprite2D
+
+var active=true
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		if velocity.y > 700:
 			velocity.y = 700
-		
-	if Input.is_action_just_pressed("jump") && is_on_floor():
-		jump(jumpforce)
-		
-
-	var direction = Input.get_axis("move_left", "move_right") 
-
-	if direction != 0:
-		player.flip_h = (direction == -1)
-		
-	velocity.x = direction * speed
+	var direction=0
+	if active:
+		if Input.is_action_just_pressed("jump") && is_on_floor():
+			jump(jumpforce)
+			
+		direction = Input.get_axis("move_left", "move_right")
+		if direction != 0:
+			player.flip_h = (direction == -1)
+			
+		velocity.x = direction * speed
 	
 	move_and_slide()
 	update_animations(direction)
-	
+
+func set_active(player_active:bool):
+	active=player_active
+	if not active:
+		velocity=Vector2.ZERO
 
 func jump(force):
 	velocity.y = -force
