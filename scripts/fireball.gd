4989a9f8618bc329849d86cd4c35440fa380ae1b
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-03-05 18:10:12 -0500

    feat: fireball timer

--- scripts/fireball.gd
+++ scripts/fireball.gd
@@ -4,10 +4,13 @@ signal touch_player
 @export var speed=240
 @onready var sprite=$Area2D/AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
+
+
 func _ready():
 	if(direction!=1):
 		sprite.flip_h=true
 	print(direction)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,3 +25,7 @@ func _on_area_2d_body_entered(body):
 
 func setDirection(newDirection):
 	direction=newDirection
+
+
+func _on_timer_timeout():
+	queue_free()
