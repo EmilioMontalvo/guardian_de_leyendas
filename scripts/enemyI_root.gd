e0e83e18bdd0312fbd1fa7fb3db54e9e89c122ed
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-03-04 19:46:54 -0500

    fix: enemyI signal

---
+++ scripts/enemyI_root.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+signal touch_player
+# Called when the node enters the scene tree for the first time.
+
+
+func _on_damage_area_body_entered(body):
+	if body is Player:
+		touch_player.emit()
